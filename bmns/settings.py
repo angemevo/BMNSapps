e80c876ce4dc686dbadef02e83b52af001ee1ae5
angemevo <mevoange903@gmail.com>
2024-06-11 22:45:28 +0000

    modif+

--- bmns/settings.py
+++ bmns/settings.py
@@ -129,7 +129,15 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "bmns/static"),
+]
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
